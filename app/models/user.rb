59689269e781af23641c6eccab8afabec9d26369
tojo-0912 <sakuramoti01414@gmail.com>
2024-03-03 19:35:26 +0900

    ユーザー管理機能(修正２)

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   validates :birthday, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates_format_of :password, with: PASSWORD_REGEX, message: '半角英数字を含めて6文字以上で設定ください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: ''
 
   has_many :items
 end
